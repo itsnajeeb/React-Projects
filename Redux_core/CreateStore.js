bf53f13e4014cff799ea331aa584dd3c2a667be1
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-08-20 23:43:15 +0530

    Understand createStore and combineReducers in redux

--- Redux_core/CreateStore.js
+++ Redux_core/CreateStore.js
@@ -1,9 +1,10 @@
-import { createStore } from "redux"
+import { createStore, bindActionCreators, combineReducers } from "redux"
 
 const ADD_TODO = "add_todo"
 const EDIT_TODO = "edit_text"
 const DELETE_TODO = "delete_todo"
-function todoReducer(state, action) {
+const ADD_USER = "add_user"
+function todoReducer(state = [], action) {
     if (action.type == ADD_TODO) {
         const todoText = action.payload.todoText;
 
@@ -31,31 +32,46 @@ function todoReducer(state, action) {
     }
 }
 
+function userReducer(state = [], action) {
+    if (action.type === "add_user") {
+        const userName = action.payload.userName;
+        return [
+            ...state,
+            { user: userName, id: (state.length === 0) ? 1 : state[state.length - 1].id + 1 }
+        ]
+    }
+    return state
+}
+
 
 //we conver action object into action method so it's called action Creator
-const addTodo = (todoText)=>{
-    dispatch({type:ADD_TODO, payload:{todoText}})
-}
-const deleteTodo=(todoId)=> {
-    dispatch({type:DELETE_TODO, payload:{todoId}})
-}
+const addTodo = (todoText) => ({ type: ADD_TODO, payload: { todoText } })
+const deleteTodo = (todoId) => ({ type: DELETE_TODO, payload: { todoId } })
+const addUser = (userName) => ({ type: ADD_USER, payload: { userName } })
 
+const reducer = combineReducers({ todo: todoReducer, user: userReducer });
 
-const { dispatch, subscribe, getState, replaceReducer } = createStore(todoReducer, [])
+const { dispatch, subscribe, getState, replaceReducer } = createStore(reducer)
 
-subscribe(()=> console.log(getState()))
+subscribe(() => console.log(getState()))
+
+const actions = bindActionCreators({ addTodo, deleteTodo,addUser }, dispatch);
+console.log("ACTIONS > ", actions);
+
 
 // dispatch({ type: ADD_TODO, payload: { todoText: "Todo-1" } });
 // dispatch({ type: ADD_TODO, payload: { todoText: "Todo-2" } });
 // dispatch({ type: ADD_TODO, payload: { todoText: "Todo-3" } });
 
 //IN PLACE OF DISPATCH OBJECT  WE CAN CALL WITH THE HELP OF FUNCTION
-addTodo("Todo-2")
-addTodo("Todo-1")
-addTodo("Todo-3")
+actions.addTodo("Todo-2")
+actions.addTodo("Todo-1")
+actions.addTodo("Todo-3")
+actions.addUser("Najeeb")
+actions.addUser("Zeeshan")
 
 // dispatch({ type: DELETE_TODO, payload: { todoId:2} });
-deleteTodo(2)
+actions.deleteTodo(2)
 
 
 
